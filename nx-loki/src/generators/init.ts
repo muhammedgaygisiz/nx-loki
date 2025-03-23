6d8c8ce5f2bf71d7d160e9ac930a44e2ca61ae86
e529454 <muhammed.gaygisiz@sbb.ch>
2025-03-23 12:36:52 +0100

    feat: add loki as dev dependency

--- nx-loki/src/generators/init.ts
+++ nx-loki/src/generators/init.ts
@@ -1,4 +1,5 @@
 import {
+  addDependenciesToPackageJson,
   ExpandedPluginConfiguration,
   formatFiles,
   NxJsonConfiguration,
@@ -7,6 +8,8 @@ import {
   updateNxJson,
 } from "@nx/devkit";
 import { InitGeneratorSchema } from "./schema";
+import { readPackageJson } from "nx/src/project-graph/file-utils";
+import { LOKI_VERSION } from "../utils/versions";
 
 const isPlugin = (plugin: string | ExpandedPluginConfiguration<unknown>) =>
   typeof plugin === "string"
@@ -23,20 +26,39 @@ const attachNxLokiPlugin = (nxJson: NxJsonConfiguration<"*" | string[]>) => [
   nxLokiPlugin,
 ];
 
+const updateNxJsonWithPlugin = async (
+  nxJson: NxJsonConfiguration<"*" | string[]>,
+  tree: Tree
+) => {
+  if (!nxJson.plugins) {
+    nxJson.plugins = [];
+  }
+
+  nxJson.plugins = attachNxLokiPlugin(nxJson);
+
+  updateNxJson(tree, nxJson);
+  await formatFiles(tree);
+};
+
+const addDependency = async (tree: Tree) => {
+  return addDependenciesToPackageJson(
+    tree,
+    {},
+    {
+      loki: LOKI_VERSION,
+    }
+  );
+};
+
 export async function initGenerator(tree: Tree, options: InitGeneratorSchema) {
   const nxJson = readNxJson(tree) || {};
   const hasPlugin = nxJson.plugins?.some((plugin) => isPlugin(plugin));
 
   if (!hasPlugin) {
-    if (!nxJson.plugins) {
-      nxJson.plugins = [];
-    }
-
-    nxJson.plugins = attachNxLokiPlugin(nxJson);
-
-    updateNxJson(tree, nxJson);
-    await formatFiles(tree);
+    await updateNxJsonWithPlugin(nxJson, tree);
   }
+
+  await addDependency(tree);
 }
 
 export default initGenerator;
---
+++ nx-loki/src/utils/versions.ts
@@ -0,0 +1,1 @@
+export const LOKI_VERSION = "0.35.1";
