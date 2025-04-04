7a39f085a644c668b8613a2749810aadc3f960e6
Muhammed <muhammed.gaygisiz@gmail.com>
2025-04-04 12:48:32 +0200

    fix: fix multiline command

--- nx-loki/src/plugin/build-test-params.ts
+++ nx-loki/src/plugin/build-test-params.ts
@@ -1,9 +1,7 @@
 import { LokiPluginOptions } from "./loki-plugin-options";
 
-export const buildTestParams = (options: LokiPluginOptions) => {
-  return `
+export const buildTestParams = (options: LokiPluginOptions) => `
     --diffingEngine looks-same 
-    --chrome-tolerance ${options.chromeTolerance || 0}
+    --chrome-tolerance ${options.chromeTolerance || 0} 
     --chrome-retries ${options.chromeRetries || 0}
-  `;
-};
+    `;
