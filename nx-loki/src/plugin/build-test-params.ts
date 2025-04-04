a01a7f7daa14a5d833c34d28c1f8fb333f52c15c
Muhammed <muhammed.gaygisiz@gmail.com>
2025-04-04 13:03:24 +0200

    fix: fix multiline execution in github actions

--- nx-loki/src/plugin/build-test-params.ts
+++ nx-loki/src/plugin/build-test-params.ts
@@ -1,7 +1,7 @@
 import { LokiPluginOptions } from "./loki-plugin-options";
 
-export const buildTestParams = (options: LokiPluginOptions) => `
-    --diffingEngine looks-same 
-    --chrome-tolerance ${options.chromeTolerance || 0} 
-    --chrome-retries ${options.chromeRetries || 0}
-    `;
+export const buildTestParams = (options: LokiPluginOptions) => ` \\
+  --diffingEngine looks-same \\
+  --chrome-tolerance ${options.chromeTolerance || 0} \\
+  --chrome-retries ${options.chromeRetries || 0}
+`;
